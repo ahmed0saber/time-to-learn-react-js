17f3c4405e0309edcd671ef6ceec1f320cc807d0
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-16 19:51:16 +0200

    Add files via upload

---
+++ working-with-npm/import-export/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from "react";
+import Header from './Header';
+
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <h2>Hi, I am an App!</h2>
+      </div>
+    )
+  }
+}
+
+export default App;
