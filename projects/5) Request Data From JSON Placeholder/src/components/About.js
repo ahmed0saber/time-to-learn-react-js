658bb07cbd93c463996d598907e3cede94451248
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-17 23:17:49 +0200

    Add files via upload

---
+++ projects/5) Request Data From JSON Placeholder/src/components/About.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class About extends Component {
+    render(){
+        return (
+        <div>
+            <h3>About</h3>
+            <p>What do you expect me to type in the about page of just a navigation bar ?</p>
+        </div>
+        )
+    }
+}
+
+export default About;
---
+++ projects/5) Request Data From JSON Placeholder/src/components/Navbar.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+import { Outlet, NavLink } from "react-router-dom";
+
+class Navbar extends Component {
+    render(){
+        return (
+        <div className="navbar">
+            <h3>React App</h3>
+            <div>
+                <nav>
+                    <NavLink to="/">Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/users">Users</NavLink>
+                </nav>
+            </div>
+            <Outlet/>
+        </div>
+        )
+    }
+}
+
+export default Navbar;
