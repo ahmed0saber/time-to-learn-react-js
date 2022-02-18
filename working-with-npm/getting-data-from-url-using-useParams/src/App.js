cc14f994b176df4858d13c3058be3524783587f1
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-18 02:12:39 +0200

    Add files via upload

---
+++ working-with-npm/getting-data-from-url-using-useParams/src/App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react"
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar"
+import Home from "./components/Home"
+import About from "./components/About"
+import NoPage from "./components/NoPage"
+import Users from "./components/Users"
+import Profile from "./components/Profile"
+
+class App extends Component {
+  render(){
+    return (
+      <BrowserRouter>
+        <div>
+          <Navbar/>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/about" element={<About/>} />
+            <Route path="/users" element={<Users/>} />
+            <Route path="/users/:id" element={<Profile/>} />
+            <Route path="*" element={<NoPage/>} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
+
+export default App;
---
+++ working-with-npm/getting-data-from-url-using-useParams/src/components/Users.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import axios from "axios"
+import { Link } from "react-router-dom";
+
+class Users extends Component {
+    state = {
+        users: []
+    }
+    componentDidMount(){
+        axios.get('https://jsonplaceholder.typicode.com/users')
+        .then(res => {
+            console.log(res)
+            this.setState({
+                users: res.data
+            })
+        })
+    }
+    render(){
+        return (
+        <div>
+            <h3>Users</h3>
+            <p>All users here are requested from fake data API.</p>
+            <div>
+                {this.state.users.map(user =>
+                    <div key={user.id} className="user-card">
+                        <p>name: {user.name}</p>
+                        <p>username: {user.username}</p>
+                        <p>email: {user.email}</p>
+                        <p>
+                            <Link to={`/users/${user.id}`}>View Profile</Link>
+                        </p>
+                    </div>
+                )}
+            </div>
+        </div>
+        )
+    }
+}
+
+export default Users;
