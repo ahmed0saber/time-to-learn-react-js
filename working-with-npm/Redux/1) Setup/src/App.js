b86763e779f35ad700f2d3a9595e7a8b829b73af
Ahmed Saber <ahmed0saber33@gmail.com>
2022-02-18 23:09:08 +0200

    Add files via upload

---
+++ working-with-npm/Redux/1) Setup/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react"
+
+class App extends Component {
+
+  render(){
+    return (
+        <div>
+          Redux App
+        </div>
+    )
+  }
+}
+
+export default App;
